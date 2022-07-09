d3676a3f3fda937ded8a422421f4213016230be5
AndreyITMetatech <rfandrey@yandex.ru>
2022-07-09 16:05:53 +0300

    Добавил решение задачи 8 - task_8

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -2,3 +2,12 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+ 
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (N > 1)
+{
+if (N % 2 == 0); 
+Console.WriteLine(N + " ");
+N = N - 1;
+}
