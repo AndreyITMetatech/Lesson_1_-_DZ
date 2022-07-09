2a02da0a833395882c92385315297b65df467555
AndreyITMetatech <rfandrey@yandex.ru>
2022-07-09 14:08:33 +0300

    Добавил решение задачи 4 - task_4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -3,3 +3,19 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+int max = 0;
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+max = a;
+
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (b > a && b > c) max = b;
+if (c > a && c > b) max = c;
+
+Console.WriteLine($"{a} , {b} , {c} max = {max}");
