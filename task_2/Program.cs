e31f96921c5facb8707c41cc3e2d21459c6d6767
AndreyITMetatech <rfandrey@yandex.ru>
2022-07-09 13:46:27 +0300

    Добавил решение задачи 2 - task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,20 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.Write("max a = ");
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.Write("max b = ");
+    Console.WriteLine(b);
+}
